cb5fe26526fbb2f9bd80fa1c46682f5b7574f494
Hamid Reza Mehrjoo <h.mehrjoo1994@gmail.com>
2025-11-11 15:43:16 +0330

    login user project

---
+++ server/config/db.js
@@ -0,0 +1,23 @@
+// server/config/db.js
+import sql from 'mssql';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
+
+export const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    console.log('✅ Connected to SQL Server');
+    return pool;
+  })
+  .catch(err => console.error('❌ DB Connection Failed:', err));
